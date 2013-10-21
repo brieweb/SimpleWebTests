5ab7abfb49cd4e4433666fdf3b90a3a7a069266f
John Reed <johnnycarlos@gmail.com>
2013-10-20 19:27:54 -0700

    Set a webdriver explicit wait

--- src/com/johnnycarlos/CraigslistSearch.java
+++ src/com/johnnycarlos/CraigslistSearch.java
@@ -9,22 +9,31 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class CraigslistSearch {
     
 	private static WebDriver driver;
     private static WebElement element;
+    private static WebDriverWait wait;
     
 	public static void main(String[] args) {
 		
         driver = new FirefoxDriver();
+        
+        wait = new WebDriverWait(driver, 10);
 		
         driver.get("http://sacramento.craigslist.org/sss/");
+        
+        driver.manage().window().maximize();
 		
-        element = driver.findElement(By.id("query"));
+        element = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("query")));       
         
         element.sendKeys("dungeons dragons");
+        
+        element.submit();
         
     
 
