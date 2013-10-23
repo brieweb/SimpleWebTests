114bc27895cc2916edf0ca7d215dfeb8df66b764
John Reed <johnnycarlos@gmail.com>
2013-10-22 21:54:46 -0700

    read user/passwd from args, print usage, refactor some

--- src/com/johnnycarlos/TwitterTests.java
+++ src/com/johnnycarlos/TwitterTests.java
@@ -11,11 +11,23 @@ public class TwitterTests {
 
 	private static WebDriver driver;
 	private static WebElement element;
+	private static String username;
+	private static String password;
 	    
     public static void main(String[] args) {
-			
+		
+    	checkArgs(args);
+    	
+    	start();
+    
+    	//end();
+	    
+    }
+
+	private static void start() {
+		
 	    driver = new FirefoxDriver();
-	        
+        
 	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 			
 	    driver.get("http://www.twitter.com");
@@ -24,10 +36,36 @@ public class TwitterTests {
 		
 	    element = driver.findElement(By.id("signin-email"));
 	    
-	    element.sendKeys("johnnycarlos@gmail.com");
+	    element.sendKeys(username);
 	    
-	    // put email and password as args
- 
-    }
-	
+	    element = driver.findElement(By.id("signin-password"));
+	    
+	    element.sendKeys(password);
+
+		
+	}
+
+	private static void checkArgs(String[] args) {
+		if( args.length == 0){
+			printHelp();
+            System.exit(0);
+		}
+		
+		username = args[0];
+		password = args[1];
+		
+	}
+
+	private static void end() {
+		System.out.println("Exiting");
+		driver.quit();
+	}
+
+	private static void printHelp() {
+		System.out.println("Usage:");
+		System.out.println("java -jar TwitterTests.jar <username> <password>");
+		
+	}
+
+
 }
